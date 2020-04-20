b77741d197bf2203410e7371c5eb2472416d85f0
Lucas Pentinsaari <kyusaki1@gmail.com>
2020-04-20 13:52:50 +0300

    Add install script for service file

---
+++ install-service.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Run this only once!
+
+# Ensure that root runs this script.
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+
+cp http-listener.service /etc/systemd/system/
+systemctl enable http-listener.service
+systemctl start http-listener.service 
